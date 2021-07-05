e0b004b1f268c6fb95478d997260d7955e11ea70
zhongpeng <859247268@qq.com>
2021-07-05 10:09:17 +0800

    feat: 首页tag栏布局完成

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,44 @@
 <template>
-  <div>
-    112233
+  <div class="home-wrapper">
+    <div class="tag-box">
+      <div class="tag-innerBox">
+        <div class="left-box">
+          <div class="tag-item" v-for="(item, index) in tagList" :key="index">
+            {{ item.tag_name }}
+          </div>
+        </div>
+        <div class="btn">标签管理</div>
+      </div>
+    </div>
+    <div class="content">
+      
+    </div>
   </div>
-  
 </template>
 
 <script>
 export default {
   layout: "default",
+  data() {
+    return {
+      tagList: [
+        {
+          tag_name: "推荐"
+        },
+        {
+          tag_name: "关注"
+        },
+        {
+          tag_name: "后端"
+        },
+        {
+          tag_name: "前端"
+        },
+        {
+          tag_name: "测试"
+        }
+      ]
+    };
+  }
 };
 </script>
